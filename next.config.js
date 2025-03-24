97fc8ff6aeb7e8521a18cfd2c4c2f0c4b36ee257
arungovindhasamygithub <arungwork63@gmail.com>
2025-03-24 10:04:38 +0530

    add the export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    output: 'export',       
+    reactStrictMode: true,   
+  };
+  
+  module.exports = nextConfig;
+  
