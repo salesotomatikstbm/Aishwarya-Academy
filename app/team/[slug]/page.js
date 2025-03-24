67f3b08e756a946b78dcd854c86f50bd087ec612
arungovindhasamygithub <arungwork63@gmail.com>
2025-03-24 10:19:02 +0530

    final

--- app/team/[slug]/page.js
+++ app/team/[slug]/page.js
@@ -1,23 +1,29 @@
 import BreadcrumbSection from "@/component/breadcrumb/BreadcrumbSection";
 import Layout from "@/component/layout/Layout";
 import TeamDetailSection from "@/component/team/TeamDetailSection";
-// import ErrorSection from "@/components/ErrorSection";
+import ErrorSection from "@/component/ErrorSection";
 import { teamData } from "@/data/Data";
+
 export const metadata = {
   title: "Aishwarya Academy",
   description: "Developed by Otomatiks",
 };
+
 export default function TeamDetails({ params }) {
   const { slug } = params;
-  const teamDesc = teamData.find((item) => item.slug === slug);
+  const teamDesc = teamData.find((item) => item.slug === slug) || null;
+
+  if (!teamDesc) {
+    console.error("Team member not found for slug:", slug); // Debugging log
+  }
+
   return (
     <Layout>
       <BreadcrumbSection header="Team Details" title="Team Details" />
       {teamDesc ? (
         <TeamDetailSection teamInfo={teamDesc} />
       ) : (
-        <TeamDetailSection teamInfo={teamDesc} />
-        // <ErrorSection type="Team Member" />
+        <ErrorSection type="Team Member" />
       )}
     </Layout>
   );
